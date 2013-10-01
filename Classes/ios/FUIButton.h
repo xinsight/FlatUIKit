a687d017ba429fc85970d10431087a7add109370
Jason Moore <jmoore@xinsight.ca>
2013-10-01 17:39:49 -0400

    add button background highlight

--- Classes/ios/FUIButton.h
+++ Classes/ios/FUIButton.h
@@ -11,6 +11,7 @@
 @interface FUIButton : UIButton
 
 @property(nonatomic, strong, readwrite) UIColor *buttonColor UI_APPEARANCE_SELECTOR;
+@property(nonatomic, strong, readwrite) UIColor *buttonHighlightColor UI_APPEARANCE_SELECTOR;
 @property(nonatomic, strong, readwrite) UIColor *shadowColor UI_APPEARANCE_SELECTOR;
 @property(nonatomic, readwrite) CGFloat shadowHeight UI_APPEARANCE_SELECTOR;
 @property(nonatomic, readwrite) CGFloat cornerRadius UI_APPEARANCE_SELECTOR;
